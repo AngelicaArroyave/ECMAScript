78042be6b57b2e8d390a0fe81f35c9be3ab12b64
DESKTOP-1TC6Q9U\USUARIO <aarroyaveduque20@gmail.com>
2023-05-05 09:11:32 -0500

    Complemento de práctica flat, map y flatmap

--- src/es10/00-flat-map.js
+++ src/es10/00-flat-map.js
@@ -2,6 +2,15 @@
 const array = [1, 2, 3, 4, [5, 6, 7, [8, 9, 0]]]
 // El número 3 hace referencia a la profundidad de la matriz
 console.log(array.flat(3)); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+console.log(array.flat(Infinity)); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 
 const array2 = [1, 2, 3, 4, 5]
 console.log(array2.flatMap(value => [value, value * 2])); // [1, 2, 2, 4,  3, 6, 4, 8, 5, 10]
+console.log(array2.map(value => [value, value * 2])); // [ [ 1, 2 ], [ 2, 4 ], [ 3, 6 ], [ 4, 8 ], [ 5, 10 ] ]
+
+const array3 = [1, 2, [3, 4, 5], 6]
+console.log(array3.flatMap(value => value * 2)); // [ 2, 4, NaN, 12 ]
+
+const cadenas = ['Nunca pares', 'de aprender']
+console.log(cadenas.map(value => value.split(' '))); // [ [ 'Nunca', 'pares' ], [ 'de', 'aprender' ] ]
+console.log(cadenas.flatMap(value => value.split(' '))); // [ 'Nunca', 'pares', 'de', 'aprender' ]
