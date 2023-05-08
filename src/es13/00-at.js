961a04fc5b09a86f0f6602c983f1d92f211e9955
DESKTOP-1TC6Q9U\USUARIO <aarroyaveduque20@gmail.com>
2023-05-08 09:41:22 -0500

    Práctica con at para obtener posiciones en arreglos

---
+++ src/es13/00-at.js
@@ -0,0 +1,4 @@
+// Obtiene la última posición del arreglo
+const numbers = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven']
+console.log(numbers[numbers.length - 1]); // Seven
+console.log(numbers.at(-1)); // Seven
