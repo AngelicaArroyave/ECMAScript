7fcb130bf4457cffc6a6c2efafae8d792fd22a63
DESKTOP-1TC6Q9U\USUARIO <aarroyaveduque20@gmail.com>
2023-05-08 08:45:06 -0500

    Práctica con separadores numéricos y replace All

---
+++ src/es12/00-numeric-separators.js
@@ -0,0 +1,3 @@
+// Numeric separators o Separadores numéricos
+const value = 100_000_000_000
+console.log(value); // 100000000000
---
+++ src/es12/01-replaceAll.js
@@ -0,0 +1,10 @@
+// Replace All
+const cadena = 'JavaScript es un lenguaje de programación'
+
+const replaceCadena = cadena.replace('JavaScript', 'TypeScript')
+console.log(replaceCadena); // TypeScript es un lenguaje de programación
+
+const newCadena = 'JavaScript es maraviloso, con JavaScript puedo crear el futuro de la web'
+
+const replaceAllCadena = newCadena.replaceAll('JavaScript', 'TypeScript')
+console.log(replaceAllCadena); // TypeScript es maraviloso, con TypeScript puedo crear el futuro de la web
